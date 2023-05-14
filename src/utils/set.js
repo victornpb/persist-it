5491b18f661e59e72d344912037b880700c9b868
victornpb <3372598+victornpb@users.noreply.github.com>
2023-05-14 04:12:51 -0300

    setValue

---
+++ src/utils/set.js
@@ -0,0 +1,15 @@
+/**
+ * Set a deep property on nested objects
+ * @param  {object}   obj  A object
+ * @param  {String}   path A path
+ * @param  {Any}      val  Anything that can be set
+ * @author Victor B. https://gist.github.com/victornpb/4c7882c1b9d36292308e
+ */
+export default function setDeepVal(obj, path, val) {
+    var props = path.split('.');
+    for (var i = 0, n = props.length - 1; i < n; ++i) {
+        obj = obj[props[i]] = obj[props[i]] || {};
+    }
+    obj[props[i]] = val;
+    return obj;
+}
